993ca8297c3541e053046ea0ffd7aeb2576f7314
michael <michaelblossom8654@gmail.com>
2024-05-04 18:38:37 +0530

    ci:added a function for handling CastError, DuplicateErrorand ValidationError

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,22 @@
+const AppError = require('./../utils/appError')
+
+const handleCastErrorDb = (err) => {
+  const message = `Invalid ${err.path}:${err.value}`
+  return new AppError(message, 400)
+}
+const handleDuplicateFieldsDb = (err) => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0]
+  // console.log(value)  note tha value above is an array
+  const message = `Duplicate field value:${value}. please use anther value`
+  return new AppError(message, 400)
+}
+
+const handleValidationErrorDb = (err) => {
+  const errors = Object.values(err.errors).map((el = el.message))
+  const message = `Invalid input data:${errors.join('. ')}`
+  return new AppError(message, 400)
+}
+
 const sendErrorDev = (err, res) => {
   // console.error(err.stack);
 
@@ -34,6 +53,12 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res)
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res)
+    //destructuring err arguement since it is not a good practice to overwrite an argurement of a function
+    let error = { ...err }
+    if (error.name === 'CastError') error = handleCastErrorDb(error)
+    if (error.code === '1100') error = handleDuplicateFieldsDb(error)
+    if (error.name === 'ValidationError') error = handleValidationErrorDb(error)
+
+    sendErrorProd(error, res)
   }
 }
