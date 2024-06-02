75e31501b31e06a0f8701f767bf0ff4d3daf476f
david405 <davidasamonye@gmail.com>
2024-06-02 15:03:45 +0100

    bug fixes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util') //builtin function for promifying token verification
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
