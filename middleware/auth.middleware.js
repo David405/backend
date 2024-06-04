cef596e1b001f20e47e1fa5003896f2247b747a6
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2024-06-05 00:27:49 +0100

    chore: updates

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken')
 const catchAsync = require('./../utils/catchAsync')
 const AppError = require('./../utils/appError')
 const { User } = require('./../models/user')
-const JWT_SECRET = process.env.SECRET_KEY
+const { JWT_SECRET } = require('../helpers/token.generator')
 
 // PROTECT MIDDLEWARE
 exports.verifyToken = catchAsync(async (req, res, next) => {
