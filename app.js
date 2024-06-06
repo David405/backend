87367f5f80e2675b93a94c12dbce88304949c2e3
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2024-06-06 08:11:08 +0100

    chore: updates on cors middleware

--- app.js
+++ app.js
@@ -166,6 +166,9 @@ const cors = require('cors')
 const http = require('http')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const { Server } = require('socket.io')
+const app = express()
+
 
 const catchAsync = require('./utils/catchAsync')
 const AppError = require('./utils/appError')
@@ -182,12 +185,23 @@ const chatSessionRouter = require('./routes/chatSession.route')
 const chatEvents = require('./utils/chatEvents')
 
 const jwt = require('jsonwebtoken')
-const { Server } = require('socket.io')
 
 const JWT_SECRET = process.env.SECRET_KEY
 
 dotenv.config({ path: './.env' })
-const app = express()
+
+
+const server = http.createServer(app)
+
+const PORT = process.env.PORT || 3000
+
+
+const io = new Server(server, {
+  cors:{
+    origin: "http://localhost:3000",
+    methods: ["GET","POST","PUT", "PATCH", "DELETE"],
+  }
+})
 
 
 // global middleware
@@ -231,15 +245,9 @@ db.once('open', () => {
 })
 
 
-const server = http.createServer(app)
-
-const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
-
-const io = new Server(server)
-
 
 const Message = mongoose.model('Message')
 const User = mongoose.model('User')
