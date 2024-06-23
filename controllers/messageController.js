91e2e8654a2fa1fd3afb9b95b476bb6bb8146cd2
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2024-06-23 12:57:24 +0100

    chore: pause redis caching for messaging

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,7 +1,8 @@
 const Message = require('../models/message')
 const catchAsync = require('../utils/catchAsync')
-const appError = require('../utils/appError')
+// const AppError = require('../utils/appError')
 const AppError = require('../utils/appError')
+const chatEvents = require('../utils/chatEvents')
 
 // functions that will filter out fields tha we dont want to update
 const filterObj = (obj, ...allowedFields) => {
@@ -13,7 +14,22 @@ const filterObj = (obj, ...allowedFields) => {
 }
 
 exports.createMessage = catchAsync(async (req, res, next) => {
-  const newMessage = await Message.create(req.body)
+  const message = {
+    message: req.body.message,
+    user: req.body.user,
+    chatSession: req.body.chatSession,
+  }
+
+  const newMessage = await Message.create(message)
+
+  const io = req.app.get('socketio')
+
+  io.to(message?.chatSession).emit(chatEvents.newMessage, {
+    message: newMessage.message,
+    user: newMessage.user,
+    chatSession: newMessage.chatSession,
+  })
+
   res.status(201).json({
     status: 'success',
     data: {
@@ -25,34 +41,41 @@ exports.createMessage = catchAsync(async (req, res, next) => {
 exports.getAllMessages = catchAsync(async (req, res, next) => {
   const { redisClient } = req.app.locals
 
-  let filter = {}
-  if (req.params.sessionId) filter = { chatSession: req.params.sessionId }
-
   const page = req.query.page * 1 || 1
   const limit = req.query.limit * 1 || 50
   const skip = (page - 1) * limit
-  const currentDate = new Date()
 
-  const cacheKey = `messages:${req.params.sessionId}:${page}:${limit}:${currentDate}`
+  // const currentDate = new Date()
 
-  const getMessages = async () => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const data = await redisClient.get(cacheKey)
-        if (!data) {
-          const freshData = await Message.find(filter).skip(skip).limit(limit)
-          redisClient.setEx(cacheKey, 3600, JSON.stringify(freshData))
-          resolve(freshData)
-        } else {
-          resolve(JSON.parse(data))
-        }
-      } catch {
-        reject(new AppError('Error Fetching Messages'))
-      }
-    })
-  }
+  // const cacheKey = `messages:${req.params.sessionId}:${page}:${limit}`
 
-  const messages = await getMessages()
+  // const getMessages = async () => {
+  //   return new Promise(async (resolve, reject) => {
+  //     try {
+  //       const data = await redisClient.get(cacheKey)
+  //       if (!data) {
+  //         const freshData = await Message.find({
+  //           chatSession: req.params.sessionId,
+  //         })
+  //           .skip(skip)
+  //           .limit(limit)
+  //         redisClient.setEx(cacheKey, 3600, JSON.stringify(freshData))
+  //         resolve(freshData)
+  //       } else {
+  //         resolve(JSON.parse(data))
+  //       }
+  //     } catch {
+  //       reject(new AppError('Error Fetching Messages'))
+  //     }
+  //   })
+  // }
+
+  // const messages = await getMessages()
+  const messages = await Message.find({
+    chatSession: req.params.sessionId,
+  })
+    .skip(skip)
+    .limit(limit)
 
   return res.status(200).json({
     results: messages.length,
@@ -68,7 +91,7 @@ exports.deleteMessage = catchAsync(async (req, res, next) => {
     $and: [{ user: req.user.id }, { _id: req.params.id }],
   })
   if (!message) {
-    return next(new appError('No message found ', 404))
+    return next(new AppError('No message found ', 404))
   }
 
   await Message.findByIdAndDelete(message.id)
@@ -84,7 +107,7 @@ exports.editMessage = catchAsync(async (req, res, next) => {
     $and: [{ user: req.user.id }, { _id: req.params.id }],
   })
   if (!message) {
-    return next(new appError('No message found ', 404))
+    return next(new AppError('No message found ', 404))
   }
 
   const newMessage = await Message.findByIdAndUpdate(message.id, filteredBody, {
